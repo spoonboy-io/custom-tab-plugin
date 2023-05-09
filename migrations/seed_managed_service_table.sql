8dec441f5de6054518926416d0f28021b52359c5
Ollie Phillips <ollie@interject.io>
2023-05-09 17:20:58 +0100

    fix sql queries

--- migrations/seed_managed_service_table.sql
+++ migrations/seed_managed_service_table.sql
@@ -1,18 +1,19 @@
 USE morpheus;
-INSERT INTO `custom_managed_services` (instance_name, service_name, service_cost)
+INSERT INTO custom_managed_services
+    (id, instance_name, service_name, service_cost)
 VALUES
-    (`DummyTechService-01` ,`Managed Service 1`, `250.00`),
-    (`DummyTechService-01` ,`Managed Service 2`, `50.00`),
-    (`DummyTechService-01` ,`Managed Service 3`, `90.00`),
-    (`DummyTechService-01` ,`Managed Service 4`, `25.00`),
-    (`DummyTechService-01` ,`Managed Service 5`, `120.00`),
-    (`DummyTechService-01` ,`Managed Service 6`, `150.00`),
-    (`DummyTechService-01` ,`Managed Service 7`, `300.00`),
-    (`DummyTechService-01` ,`Managed Service 8`, `80.00`),
-    (`DummyTechService-01` ,`Managed Service 9`, `75.00`),
-    (`DummyTechService-01` ,`Managed Service 10`, `50.00`),
-    (`DummyTechService-01` ,`Managed Service 11`, `40.00`),
-    (`DummyTechService-01` ,`Managed Service 12`, `1250.00`),
-    (`DummyTechService-01` ,`Managed Service 13`, `200.00`),
-    (`DummyTechService-01` ,`Managed Service 14`, `10.00`),
-    (`DummyTechService-01` ,`Managed Service 15`, `100.00`);
+    (NULL, "DummyTechService-01","Managed Service 1", "250.00"),
+    (NULL, "DummyTechService-01" ,"Managed Service 2", "50.00"),
+    (NULL, "DummyTechService-01" ,"Managed Service 3", "90.00"),
+    (NULL, "DummyTechService-01" ,"Managed Service 4", "25.00"),
+    (NULL, "DummyTechService-01" ,"Managed Service 5", "120.00"),
+    (NULL, "DummyTechService-01" ,"Managed Service 6", "150.00"),
+    (NULL, "DummyTechService-01" ,"Managed Service 7", "300.00"),
+    (NULL, "DummyTechService-01" ,"Managed Service 8", "80.00"),
+    (NULL, "DummyTechService-01" ,"Managed Service 9", "75.00"),
+    (NULL, "DummyTechService-01" ,"Managed Service 10", "50.00"),
+    (NULL, "DummyTechService-01" ,"Managed Service 11", "40.00"),
+    (NULL, "DummyTechService-01" ,"Managed Service 12", "1250.00"),
+    (NULL, "DummyTechService-01" ,"Managed Service 13", "200.00"),
+    (NULL, "DummyTechService-01" ,"Managed Service 14", "10.00"),
+    (NULL, "DummyTechService-01" ,"Managed Service 15", "100.00");
